2c5879b6e63f5433e69e8bfd040555f71dc52f59
jtrauque <julie.trauque@gmail.com>
2021-04-18 15:05:01 +0200

    update : new version with extern function - both right

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,24 +1,15 @@
 
 
 global ft_strcpy
+extern	ft_strlen
 
-ft_strlen:
-	mov rax, rsi
-	mov rbx, 0
-	call _loop
-
-_loop:
-	inc rax
-	inc rbx
-	mov cl, [rax]
-	cmp cl, 0
-	jne _loop
-	mov rcx, rbx
-
-	ret 
 
 ft_strcpy:
+	push rdi
+	mov rdi, rsi
 	call ft_strlen
+	mov rcx, rax
+	pop rdi
 	rep movsq
 	mov rax, rdi
 
