9ab665825d98c51307f1b211992249bc977a7cbc
jtrauque <julie.trauque@gmail.com>
2021-04-19 13:19:53 +0200

    clean

---
+++ Makefile
@@ -0,0 +1,56 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jtrauque <jtrauque@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/11/16 11:44:52 by jtrauque          #+#    #+#              #
+#    Updated: 2021/04/19 13:09:26 by jtrauque         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME		= libasm.a
+
+SRCS 		= ft_strlen.s ft_strcpy.s ft_strcmp.s ft_write.s
+
+SRC_DIR   = srcs/
+OBJ_DIR   = objs/
+
+OBJS      = $(addprefix $(OBJ_DIR), ${SRCS:.s=.o})
+DIR		  = $(sort $(dir $(OBJS)))
+
+DEP       = ${OBJS:.o=.d}
+
+NASM		= nasm -f elf64
+
+RM		= rm -rf
+
+MAKE	+= --no-print-directory
+
+all :
+	@${MAKE} -j ${NAME}
+ 
+$(NAME):	${OBJS}
+		@ar rcs libasm.a ${OBJS}
+$(DIR) :    
+	@mkdir -p $@
+
+$(OBJ_DIR)%.o: $(SRC_DIR)%.s | $(DIR)
+		@$(NASM) $< -o $@
+test : all
+		@gcc main.c libasm.a
+		@./a.out
+
+clean :
+		@${RM} ${OBJ_DIR}
+
+fclean : 	clean
+		@${RM} libasm.a
+
+re : 	fclean 
+		@${MAKE} all
+
+-include ${OBJS:.o=.d} 
+
+.PHONY : 	all bonus clean fclean re test
--- main.c
+++ main.c
@@ -1,10 +1,38 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
+#define strcmp_test(s, s2) \
+{ \
+	char *str = s; \
+	char *dst = s2; \
+	int ret_true = strcmp(str, dst); \
+	int ret = ft_strcmp(str, dst); \
+	if (ret != ret_true) \
+	{ \
+		printf("%s vs %s\n", str, dst); \
+		printf("%d vs %d\n", ret, ret_true); \
+	} \
+	else \
+		printf("✔\n"); \
+}
+
+#define write_test(fd, str, len) \
+{ \
+	int ret_true = write(fd, str, len); \
+	int ret = ft_write(fd, str, len); \
+	if (ret != ret_true) \
+	{ \
+		printf("%d vs %d\n", ret, ret_true); \
+	} \
+	else \
+		printf("✔\n"); \
+}
 int		ft_strlen(char *str);
 char	*ft_strcpy(char *dest, const char *str);
 int		ft_strcmp(char *str, char *dst);
+int		ft_write(int fd, const void* buf, size_t count);
 
 /* int		main() */
 /* { */
@@ -24,12 +52,25 @@ int		ft_strcmp(char *str, char *dst);
 /* 	printf("%s\n", dst); */
 /* } */
 
+/* int		main() */
+/* { */
+/* 	strcmp_test("Hel", "Hello"); */
+/* 	strcmp_test("Hello", "Hel"); */
+/* 	strcmp_test("Hello", "Hello"); */
+/* 	strcmp_test("Hello", "Hcllo"); */
+/* 	strcmp_test("Hcllo", "Hello"); */
+/* 	strcmp_test("Hello", ""); */
+/* 	strcmp_test("", "Hello"); */
+/* 	strcmp_test("", ""); */
+/* } */
+
 int		main()
 {
-	char *str = "Hello World";
-	char *dst = "Hcllo World";
-	int ret;
-
-	ret = ft_strcmp(str, dst);
-	printf("%d vs %d\n", ret, strcmp(str, dst));
+	write_test(1, "Hello World\n", 12);
+	write_test(1, "Hello World\n", 6);
+	write_test(1, "Hello World\n", 14);
+	write_test(1, "Hello World\n", 0);
+	write_test(1, "", 0);
+	write_test(1, "", 2);
+	write_test(2, "erreur", 6);
 }
